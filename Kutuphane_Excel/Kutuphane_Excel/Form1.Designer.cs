ad4bf7d8addbbffc1842422a5f88e1895ae85a65
Gülistan <gulistanakgul9@gmail.com>
2024-02-18 01:02:56 +0300

    kütüphane uygulaması
    winform arayüzü ile  kitap bilgilerini excel dosyasına aktaran,silme ve güneclleme işlemleri yapan  basit bir kütüphane uygulaması

---
+++ Kutuphane_Excel/Kutuphane_Excel/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Kutuphane_Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgwKitaplar = new System.Windows.Forms.DataGridView();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txbYayinEvi = new System.Windows.Forms.TextBox();
+            this.txbTuru = new System.Windows.Forms.TextBox();
+            this.txbYazar = new System.Windows.Forms.TextBox();
+            this.txbSayfaSayisi = new System.Windows.Forms.TextBox();
+            this.txbKitapAdi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwKitaplar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dgwKitaplar);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(95)))), ((int)(((byte)(95)))));
+            this.splitContainer1.Panel2.Controls.Add(this.buttonKaydet);
+            this.splitContainer1.Panel2.Controls.Add(this.btnEkle);
+            this.splitContainer1.Panel2.Controls.Add(this.txbYayinEvi);
+            this.splitContainer1.Panel2.Controls.Add(this.txbTuru);
+            this.splitContainer1.Panel2.Controls.Add(this.txbYazar);
+            this.splitContainer1.Panel2.Controls.Add(this.txbSayfaSayisi);
+            this.splitContainer1.Panel2.Controls.Add(this.txbKitapAdi);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(804, 450);
+            this.splitContainer1.SplitterDistance = 465;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dgwKitaplar
+            // 
+            this.dgwKitaplar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwKitaplar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgwKitaplar.Location = new System.Drawing.Point(0, 0);
+            this.dgwKitaplar.Name = "dgwKitaplar";
+            this.dgwKitaplar.Size = new System.Drawing.Size(465, 450);
+            this.dgwKitaplar.TabIndex = 0;
+            this.dgwKitaplar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dgwKitaplar_MouseDown);
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(131)))), ((int)(((byte)(120)))));
+            this.buttonKaydet.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.buttonKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonKaydet.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKaydet.Location = new System.Drawing.Point(138, 45);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(155, 62);
+            this.buttonKaydet.TabIndex = 11;
+            this.buttonKaydet.Text = "Kaydet";
+            this.buttonKaydet.UseVisualStyleBackColor = false;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEkle.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEkle.Location = new System.Drawing.Point(136, 329);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(157, 30);
+            this.btnEkle.TabIndex = 10;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txbYayinEvi
+            // 
+            this.txbYayinEvi.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txbYayinEvi.Location = new System.Drawing.Point(136, 285);
+            this.txbYayinEvi.Name = "txbYayinEvi";
+            this.txbYayinEvi.Size = new System.Drawing.Size(157, 22);
+            this.txbYayinEvi.TabIndex = 9;
+            // 
+            // txbTuru
+            // 
+            this.txbTuru.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txbTuru.Location = new System.Drawing.Point(136, 247);
+            this.txbTuru.Name = "txbTuru";
+            this.txbTuru.Size = new System.Drawing.Size(157, 22);
+            this.txbTuru.TabIndex = 8;
+            // 
+            // txbYazar
+            // 
+            this.txbYazar.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txbYazar.Location = new System.Drawing.Point(136, 175);
+            this.txbYazar.Name = "txbYazar";
+            this.txbYazar.Size = new System.Drawing.Size(157, 22);
+            this.txbYazar.TabIndex = 7;
+            // 
+            // txbSayfaSayisi
+            // 
+            this.txbSayfaSayisi.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txbSayfaSayisi.Location = new System.Drawing.Point(136, 209);
+            this.txbSayfaSayisi.Name = "txbSayfaSayisi";
+            this.txbSayfaSayisi.Size = new System.Drawing.Size(157, 22);
+            this.txbSayfaSayisi.TabIndex = 6;
+            // 
+            // txbKitapAdi
+            // 
+            this.txbKitapAdi.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txbKitapAdi.Location = new System.Drawing.Point(136, 139);
+            this.txbKitapAdi.Name = "txbKitapAdi";
+            this.txbKitapAdi.Size = new System.Drawing.Size(157, 22);
+            this.txbKitapAdi.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(31, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Yayın Evi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(33, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Türü";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(31, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sayfa Sayısı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(33, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yazar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitap Adı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwKitaplar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbSayfaSayisi;
+        private System.Windows.Forms.TextBox txbKitapAdi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbYayinEvi;
+        private System.Windows.Forms.TextBox txbTuru;
+        private System.Windows.Forms.TextBox txbYazar;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.DataGridView dgwKitaplar;
+        private System.Windows.Forms.Button buttonKaydet;
+    }
+}
+
---
+++ Kutuphane_Excel/Kutuphane_Excel/Form1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Kutuphane_Excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool formYüklendi = false;
+      
+        string dosyaAdi = Path.Combine(Directory.GetCurrentDirectory(), "kitaplar_excel.xlsx");
+        string kitapAdi;
+        string yazar;
+        string sayfaSayisi;
+        string turu;
+        string yayinEvi;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            dgwKitaplar.ColumnCount = 5;
+            dgwKitaplar.Columns[0].HeaderText = "Kitap Adı";
+            dgwKitaplar.Columns[1].HeaderText = "Yazar";
+            dgwKitaplar.Columns[2].HeaderText = "Sayfa sayısı";
+            dgwKitaplar.Columns[3].HeaderText = "Türü";
+            dgwKitaplar.Columns[4].HeaderText = "Yayın Evi";
+            dgwKitaplar.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            if (!File.Exists(dosyaAdi))
+            {
+                DosyaOlustur(dosyaAdi, dgwKitaplar);
+            }
+            else
+            {
+                DgwAktar();
+            }
+            formYüklendi = true;
+
+        }
+
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+
+            kitapAdi = txbKitapAdi.Text;
+            yazar = txbYazar.Text;
+            sayfaSayisi = txbSayfaSayisi.Text;
+            turu = txbTuru.Text;
+            yayinEvi = txbYayinEvi.Text;
+            if (string.IsNullOrEmpty(kitapAdi) || string.IsNullOrEmpty(yazar) || string.IsNullOrEmpty(sayfaSayisi) ||
+                string.IsNullOrEmpty(turu) || string.IsNullOrEmpty(yayinEvi))
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurunuz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else
+            {
+                dgwKitaplar.Rows.Add(kitapAdi, yazar, sayfaSayisi, turu, yayinEvi);
+            }
+            
+
+        }
+      
+
+
+        static void DosyaOlustur(string dosyaAdi, DataGridView dgwKitaplar)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Add();
+            Excel.Worksheet worksheet = workbook.Worksheets[1];
+
+            for (int i = 0; i < dgwKitaplar.Columns.Count; i++)
+            {
+                worksheet.Cells[1, i + 1] = dgwKitaplar.Columns[i].HeaderText;
+            }
+
+            workbook.SaveAs(dosyaAdi);
+            workbook.Close();
+            excelApp.Quit();
+        }
+
+        private void DgwAktar()
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(dosyaAdi);
+            Excel.Worksheet worksheet = workbook.Worksheets[1];
+            int sonSatir = worksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+
+            dgwKitaplar.Rows.Clear();
+
+            for (int satir = 1; satir <= sonSatir; satir++)
+            {
+                kitapAdi = worksheet.Cells[satir, 1].Value?.ToString();
+                yazar = worksheet.Cells[satir, 2].Value?.ToString();
+                sayfaSayisi = worksheet.Cells[satir, 3].Value?.ToString();
+                turu = worksheet.Cells[satir, 4].Value?.ToString();
+                yayinEvi = worksheet.Cells[satir, 5].Value?.ToString();
+
+                dgwKitaplar.Rows.Add(kitapAdi, yazar, sayfaSayisi, turu, yayinEvi);
+            }
+            workbook.Close();
+            excelApp.Quit();
+        }
+
+        private void ExcelKayit()
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(dosyaAdi);
+            Excel.Worksheet worksheet = workbook.Worksheets[1];
+            if (worksheet.Rows.Count > 1)
+            {
+                worksheet.Rows[1].Delete();
+            }
+
+            for (int satir = 0; satir < dgwKitaplar.Rows.Count; satir++)
+            {
+                for (int sutun = 0; sutun < dgwKitaplar.Columns.Count; sutun++)
+                {
+                    worksheet.Cells[satir + 1, sutun + 1] = dgwKitaplar.Rows[satir].Cells[sutun].Value;
+                }
+            }
+            workbook.Save();
+            workbook.Close();
+            excelApp.Quit();
+
+            MessageBox.Show("Değişiklikler Excel dosyasına kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            if (formYüklendi)
+                ExcelKayit();
+        }
+
+        private void dgwKitaplar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var satir = dgwKitaplar.HitTest(e.X,e.Y);
+                if(satir.RowIndex >=0 && satir.RowIndex < dgwKitaplar.Rows.Count)
+                {
+                    dgwKitaplar.ClearSelection();
+                    dgwKitaplar.Rows[satir.RowIndex].Selected = true;
+                    DialogResult result= MessageBox.Show("Seçili Kaydı Silmek İstiyor Musunuz?","Kayıt Silme",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                
+                    if(result == DialogResult.Yes)
+                    {
+                        dgwKitaplar.Rows.RemoveAt(satir.RowIndex);
+                    }
+                }
+            }
+        }
+    }
+}
